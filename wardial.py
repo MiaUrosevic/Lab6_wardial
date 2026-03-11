1a0dd1c9fbafc772df78fddee27e389461747793
Mia Urosevic <miaurosevic21@gmail.com>
2026-03-10 21:40:58 -0700

    lab6

--- wardial.py
+++ wardial.py
@@ -5,28 +5,22 @@ Complete the lab by fixing the FIXME annotations below.
 
 import requests
 
-
 def is_server_at_hostname(hostname):
     '''
-    A hostname is a generic word for either an IP address or a domain name.
-    Your function should return True if `requests.get` is successfully able to connect to the input hostname.
-
     >>> is_server_at_hostname('google.com')
     True
     >>> is_server_at_hostname('www.google.com')
     True
     >>> is_server_at_hostname('GoOgLe.CoM')
     True
-    >>> is_server_at_hostname('142.250.68.110')  # IP address for google.com
+    >>> is_server_at_hostname('142.250.68.110')
     True
-
     >>> is_server_at_hostname('facebook.com')
     True
     >>> is_server_at_hostname('www.facebook.com')
     True
     >>> is_server_at_hostname('FACEBOOK.com')
     True
-
     >>> is_server_at_hostname('google.commmm')
     False
     >>> is_server_at_hostname('aslkdjlaksjdlaksjdlakj')
@@ -37,16 +31,14 @@ def is_server_at_hostname(hostname):
     False
     '''
     try:
-        requests.get('http://' + hostname, timeout=5)
+        r = requests.get('http://' + hostname, timeout=5)
         return True
-    except requests.exceptions.RequestException:
+    except Exception:
         return False
 
 
 def increment_ip(ip):
     '''
-    Return the "next" IPv4 address.
-
     >>> increment_ip('1.2.3.4')
     '1.2.3.5'
     >>> increment_ip('1.2.3.255')
@@ -65,36 +57,23 @@ def increment_ip(ip):
     '0.0.0.0'
     '''
     parts = [int(x) for x in ip.split('.')]
-
     parts[3] += 1
-    if parts[3] > 255:
-        parts[3] = 0
-        parts[2] += 1
-    if parts[2] > 255:
-        parts[2] = 0
-        parts[1] += 1
-    if parts[1] > 255:
-        parts[1] = 0
-        parts[0] += 1
-    if parts[0] > 255:
-        parts[0] = 0
-
-    return f'{parts[0]}.{parts[1]}.{parts[2]}.{parts[3]}'
+    for i in range(3, 0, -1):
+        if parts[i] == 256:
+            parts[i] = 0
+            parts[i-1] += 1
+    parts[0] = parts[0] % 256
+    return '.'.join(str(p) for p in parts)
 
 
 def enumerate_ips(start_ip, n):
     '''
-    Return a list containing the next `n` IPs beginning with `start_ip`.
-
     >>> list(enumerate_ips('192.168.1.0', 2))
     ['192.168.1.0', '192.168.1.1']
-
     >>> list(enumerate_ips('8.8.8.8', 10))
     ['8.8.8.8', '8.8.8.9', '8.8.8.10', '8.8.8.11', '8.8.8.12', '8.8.8.13', '8.8.8.14', '8.8.8.15', '8.8.8.16', '8.8.8.17']
-
     >>> list(enumerate_ips('192.168.0.255', 2))
     ['192.168.0.255', '192.168.1.0']
-
     >>> len(list(enumerate_ips('8.8.8.8', 10)))
     10
     >>> len(list(enumerate_ips('8.8.8.8', 1000)))
@@ -109,15 +88,14 @@ def enumerate_ips(start_ip, n):
 
 
 if __name__ == '__main__':
-    # scan the DPRK network block
-    # (175.45.176.0 – 175.45.176.255)
-    dprk_ips = list(enumerate_ips('175.45.176.0', 256))
 
+    # FIXME 1: Generate all DPRK IPs
+    dprk_ips = list(enumerate_ips('175.45.176.0', 1024))
+
+    # FIXME 2: Filter to only IPs with web servers
     dprk_ips_with_servers = []
-
     for ip in dprk_ips:
-        print(f'scanning {ip} ...')   # optional but recommended
-
+        print('scanning:', ip)
         if is_server_at_hostname(ip):
             dprk_ips_with_servers.append(ip)
 
